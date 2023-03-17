c411241951e646bd761373c4539c950f7cbe8630
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-03-17 19:28:11 +0630

    refactor(database): using singleton with prisma and controlling connection and disconnect
    Prisma Js modified when app started launch and we need to check db connect or not and when server
    shut down automatically disconnect database.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import helmet from "helmet";
+import prisma from "./lib/prisma";
+
+/*=============================================
+=        All Library Module Imported          =
+=============================================*/
 import logger, { notFoundLog, errorLog } from "./lib/logger";
 
 /*=============================================
@@ -23,49 +28,62 @@ dotenv.config({ path: path.join("../", ".env") });
 
 const app: Express = express();
 
-/**
- *  Express.js security with HTTP headers
- *  It's not a silver bullet, but it can help!
- *  @doc : https://helmetjs.github.io/
- *
- */
-app.use(helmet());
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Database connection Started");
+    /**
+     *  Express.js security with HTTP headers
+     *  It's not a silver bullet, but it can help!
+     *  @doc : https://helmetjs.github.io/
+     *
+     */
+    app.use(helmet());
 
-/**
- * HTTP request logger middleware for node.js
- * @doc : https://github.com/expressjs/morgan#readme
- */
-app.use(logger);
-app.use(notFoundLog);
-app.use(errorLog);
+    /**
+     * HTTP request logger middleware for node.js
+     * @doc : https://github.com/expressjs/morgan#readme
+     */
+    app.use(logger);
+    app.use(notFoundLog);
+    app.use(errorLog);
 
-/**
- * First Server Configuration Data
- * Response
- */
-app.get("/", (req: Request, res: Response) => {
-  res.json(serverConfig);
-});
+    /**
+     * First Server Configuration Data
+     * Response
+     */
+    app.get("/", (req: Request, res: Response) => {
+      res
+        .status(200)
+        .json({ errorCode: 0, data: serverConfig, message: "Server Info" });
+    });
 
-/**
- * =================================================
- * Router System.
- * =================================================
- *
- * @description : defines all routes as RootRoute
- * @folder '/src/routes/RouteIndex'
- */
-setRootRouter(app);
+    /**
+     * =================================================
+     * Router System.
+     * =================================================
+     *
+     * @description : defines all routes as RootRoute
+     * @folder '/src/routes/RouteIndex'
+     */
+    setRootRouter(app);
 
-app.use("*", (req: Request, res: Response) => {
-  res.status(404).json({
-    errorCode: 0,
-    message: "Request Not Found 404",
-    statusCode: 404,
-  });
-});
+    app.use("*", (req: Request, res: Response) => {
+      res.status(404).json({
+        errorCode: 0,
+        message: "Request Not Found 404",
+        statusCode: 404,
+      });
+    });
 
-// Listening on http://localhost:{serverConfig.port}
-app.listen(serverConfig.port, () => {
-  console.log(`Now Listening on Port ${serverConfig.port}`);
+    // Listening on http://localhost:{serverConfig.port}
+    app.listen(serverConfig.port, () => {
+      console.log(`Now Listening on Port ${serverConfig.port}`);
+    });
+  })
+  .catch(console.log);
+
+prisma.$on("beforeExit", () => {
+  console.log("Disconnecting from Database");
+  prisma.$disconnect();
 });
